c70ddead2ffc86a252ab947657adc5112a173233
Fernando Coltri <fcoltri@gmail.com>
2024-11-18 16:22:31 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome("Fernando");
+
+        Conta cc = new ContaCorrente(cliente);
+        Conta poupanca = new ContaPoupanca(cliente);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+
+}
